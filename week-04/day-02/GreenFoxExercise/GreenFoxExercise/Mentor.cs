eaa7c27436693c7b21b33b43209bf846127d9d33
Marek <marekpoliak0@gmail.com>
2019-10-01 11:20:34 +0200

    add Mentor class

---
+++ week-04/day-02/GreenFoxExercise/GreenFoxExercise/Mentor.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GreenFoxExercise
+{
+    class Mentor:Person
+    {
+        public level Level { get; set; }
+
+        public Mentor(string name, int age, bool isMale,level level) : base(name, age, isMale)
+        {
+            Level = level;
+        }
+        
+        public Mentor() : base()
+        {
+            Level = level.intermediate;
+        }
+
+        public void GetGoal()
+        {
+            Console.WriteLine("Educate brilliant junior software developers.");
+        }
+
+        public void Introduce()
+        {
+            Console.WriteLine("Hi, I'm "+Name+Age+Level); //whatever
+        }
+    }
+    enum level
+    {
+        junior,
+        intermediate,
+        senior
+    }
+}
