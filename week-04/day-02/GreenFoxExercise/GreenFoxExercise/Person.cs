293408a29582f5857d7799cf3e92c3494cbc3d02
Marek <marekpoliak0@gmail.com>
2019-10-01 10:51:46 +0200

    add student and person class

---
+++ week-04/day-02/GreenFoxExercise/GreenFoxExercise/Person.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GreenFoxExercise
+{
+    class Person
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+
+        public bool IsMale { get; set; }
+
+        public Person(string name,int age, bool isMale)
+        {
+            Name = name;
+            Age = age;
+            IsMale = isMale;
+            Console.ReadLine();
+        }
+        public Person()
+        {
+            Name = "JaneDoe";
+            Age = 30;
+            IsMale = false;
+        }
+        
+        public void Introduce()
+        {
+            Console.WriteLine("Hi...whatever :D"+Name+Age+IsMale);
+        }
+        public void GetGoal()
+        {
+            Console.WriteLine("My goal is: Live for the moment!");
+        }
+    }
+}
---
+++ week-04/day-02/GreenFoxExercise/GreenFoxExercise/Student.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GreenFoxExercise
+{
+    class Student : Person
+    {
+        private string previousOrganization;
+        private int skippedDays;
+
+        public Student(string name, int age, bool isMale, string previousOrganization) : base(name, age, isMale)
+        {
+            this.previousOrganization = previousOrganization;
+            skippedDays = 0;
+        }
+
+        public Student() : base()
+        {
+            previousOrganization = "The School of Life";
+            skippedDays = 0;
+        }
+            
+    }
+}
