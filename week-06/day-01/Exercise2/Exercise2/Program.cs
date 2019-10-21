bb89c77fd5aec0a8b062fa21b694a2ad54f1cbad
Marek <marekpoliak0@gmail.com>
2019-10-21 11:00:54 +0200

    add exercise 2

---
+++ week-06/day-01/Exercise2/Exercise2/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Linq;
+
+namespace Exercise2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] n = { 1, 3, -2, -4, -7, -3, -8, 12, 19, 6, 9, 10, 14 };
+            int averageOfOdds = n.Where(num => num % 2 == 0).Sum() / n.Where(num=>num%2==0).Count();
+            Console.WriteLine(averageOfOdds);
+        }
+    }
+}
