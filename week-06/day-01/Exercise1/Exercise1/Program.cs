e748616b5a8595fafffe784757de30ed83ae5c21
Marek <marekpoliak0@gmail.com>
2019-10-21 10:38:46 +0200

    add first exercise

---
+++ week-06/day-01/Exercise1/Exercise1/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Linq;
+
+namespace Exercise1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] n = { 1, 3, -2, -4, -7, -3, -8, 12, 19, 6, 9, 10, 14 };
+            var evenNums = n.Where(num => num % 2 == 0);
+            evenNums = from num in n
+                       where num % 2 == 0
+                       select num;
+        }
+    }
+}
