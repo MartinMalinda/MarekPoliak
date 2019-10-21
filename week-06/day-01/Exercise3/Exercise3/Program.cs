ece0e27f3743a35c4297285d9785a435f5de2f50
Marek <marekpoliak0@gmail.com>
2019-10-21 11:19:41 +0200

    add exercise 3

---
+++ week-06/day-01/Exercise3/Exercise3/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Linq;
+
+namespace Exercise3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] n = { 1, 3, -2, -4, -7, -3, -8, 12, 19, 6, 9, 10, 14 };
+            var squaredPositive = n.Where(num => num > 0).Select(num => num * num);
+            foreach (var item in squaredPositive)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
