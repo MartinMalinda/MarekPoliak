613cec48f3b55a43c2576b055ca0b555ea9e30c6
Marek <marekpoliak0@gmail.com>
2019-10-21 12:57:05 +0200

    add exercise 11

---
+++ week-06/day-01/Exercise11/Exercise11/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Linq;
+using System.IO;
+
+namespace Exercise11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string contents = File.ReadAllText(@"text.txt");
+            var splitted = contents.Split(new char[] { ' ', '\n', '\r' }, StringSplitOptions.RemoveEmptyEntries);
+            var words = splitted.Where(s => s.ToLower().All(c => (c > 96) && (c < 123)));
+            var occurences = words.GroupBy(w => w);
+            var orderedOccurences = occurences.OrderByDescending(w => w.Count());
+            var topOccurences = orderedOccurences.Take(100);
+            topOccurences.ToList().ForEach(s => Console.WriteLine(s.Key + " - " + s.Count()));
+        }
+    }
+}
