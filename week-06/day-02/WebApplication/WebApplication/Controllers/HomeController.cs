343666a7fec98af74be895b02501be0d779c7ace
Marek <marekpoliak0@gmail.com>
2019-10-22 17:30:07 +0200

    add webapp

---
+++ week-06/day-02/WebApplication/WebApplication/Controllers/HomeController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication.Controllers
+{
+    public class HomeController : Controller
+    {
+
+        [Route("")]
+        public string Index()
+        {
+            return "Hello from the Controller!";
+        }
+
+    }
+}
---
+++ week-06/day-02/WebApplication/WebApplication/Controllers/RestController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using WebApplication.Models;
+
+namespace WebApplication.Controllers
+{
+    [Route("api")]
+    public class RestController : Controller
+    {
+        public static int IdCounter { get; set; } = 0;
+        public Greeting greeting { get; set; } = new Greeting();
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [Route("greeting")]
+        [HttpGet]
+        public Greeting Greet()
+        {
+            Greeting greeting = new Greeting();
+            IdCounter++;
+            greeting.Id = IdCounter;
+            greeting.Content = Request.Query["name"];
+
+            return greeting;
+        }
+    }
+}
