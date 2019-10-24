de1c35492d9e7546745bccc3efef56f7d99cfb48
Marek <marekpoliak0@gmail.com>
2019-10-24 14:29:30 +0200

    add webshop exercise

---
+++ week-06/day-04/Webshop/Webshop/Classes/ShopItem.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Webshop.Classes
+{
+    public class ShopItem
+    {
+        public List<string> Attributes { get; set; }
+        public int NumOfAttributes { get; set; } = 4;
+
+        public ShopItem(string name, string description, string price, string quantity)
+        {
+            Attributes = new List<string>();
+            Attributes.Add(name);
+            Attributes.Add(description);
+            Attributes.Add(price);
+            Attributes.Add(quantity);
+        }
+    }
+}
---
+++ week-06/day-04/Webshop/Webshop/Models/IndexModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Webshop.Classes;
+
+namespace Webshop.Models
+{
+    public class IndexModel
+    {
+        public string Filter { get; set; }
+        public string Search { get; set; }
+        public List<ShopItem> List { get; set; } = new List<ShopItem>();
+
+        public IndexModel()
+        {
+            List.Add(new ShopItem("Running Shoes", "Nike running shoes for every day sport", "1000", "5"));
+            List.Add(new ShopItem("Printer", "Some HP printer that will print pages", "3000", "2"));
+            List.Add(new ShopItem("Coca cola", "0.5l standard coke", "25", "10"));
+            List.Add(new ShopItem("T-shirt", "blue with a corgi on a bike", "300", "1"));
+        }
+    }
+}
