52f82fc2fa5c0bcd1535e229510c64b19be74ffe
Marek <marekpoliak0@gmail.com>
2019-10-29 11:59:00 +0100

    add full workshop

---
+++ week-07/day-02/BankAccount/BankAccount/Models/ListModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BankAccount.Models
+{
+    public static class ListModel
+    {
+        public static List<AnimalsAccount> list { get; set; } = new List<AnimalsAccount>()
+        {
+            new AnimalsAccount("good guy", "simba", 1000, "lion", true),
+            new AnimalsAccount("bad guy", "idk", 2543, "idk", false),
+            new AnimalsAccount("bad guy", "marek", 543, "pig", false)
+        };
+    }
+}
